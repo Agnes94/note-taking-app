966846fa33fc15b329a3bf9a13a3b10ff743cb10
Agnes94 <agnes.ketesdi@gmail.com>
2022-07-26 20:12:22 +0200

    change tsconfig and clean up file structure

---
+++ notes/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SidePanel } from './components/sidePanel';
+
+export const App = () => {
+  return (
+    <div>
+      Hello!
+      <SidePanel/>
+    </div>
+  );
+}
+
---
+++ notes/src/components/sidePanel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const SidePanel = () => {
+  return (
+    <div>
+      Panel
+    </div>
+  );
+}
+
