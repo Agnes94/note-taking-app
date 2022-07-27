48a1bb4214275f5e45f1a19b5b38d60ce3bec5de
Agnes94 <agnes.ketesdi@gmail.com>
2022-07-27 21:37:00 +0200

    set up side panel and restructure button component

--- notes/src/components/button.tsx
+++ notes/src/components/button.tsx
@@ -1,57 +1,6 @@
 import React, { FC } from 'react';
 import styled from "styled-components";
 
-
-const StyledButton = styled.button<ButtonProps>`
-  position:        relative;
-  justify-content: center;
-  text-align:      center;
-  align-items:     center;
-  display:         inline-flex;
-  flex-direction:  row;
-  text-transform:  uppercase;
-  border:          none;
-  cursor:          pointer;
-  font-weight:     500;
-  color:           'blue';
-  border-radius:   4px;
-  padding:         1rem 1rem;
-  min-width:       fit-content;
-
-  &:disabled {
-    cursor:     not-allowed;
-    background: darkblue;
-    color:      darkblue;
-    opacity:    0.7;
-    outline:    none;
-    border:     none;
-  }
-
-  &:active {
-    transform: translateY(0.5px);
-  }
-`;
-
-
-type ButtonProps = {
-  children?:     any;
-  className?:    string;
-  disabled?:     boolean;
-  hide?:         boolean;
-  isLoading?:    boolean;
-  onClick?:      React.MouseEventHandler<HTMLButtonElement>;
-  open?:         boolean;
-};
-
-const InnerContent = styled.div`
-  display:         flex;
-  flex-direction:  row;
-  position:        relative;
-  align-items:     center;
-  justify-content: center;
-  gap:             0.5rem;
-`;
-
 export const Button: FC<ButtonProps> = ({
   disabled     = false,
   isLoading    = false,
@@ -70,3 +19,56 @@ export const Button: FC<ButtonProps> = ({
     </StyledButton>
   );
 };
+
+  const StyledButton = styled.button<ButtonProps>`
+    position:        fixed;
+    bottom:          0;
+    right:           0;
+    justify-content: center;
+    text-align:      center;
+    align-items:     center;
+    display:         inline-flex;
+    flex-direction:  row;
+    text-transform:  uppercase;
+    border:          none;
+    cursor:          pointer;
+    font-weight:     500;
+    color:           'blue';
+    border-radius:   4px;
+    padding:         1rem 1rem;
+    margin:          3rem 3rem;
+    min-width:       fit-content;
+  
+    &:disabled {
+      cursor:     not-allowed;
+      background: darkblue;
+      color:      darkblue;
+      opacity:    0.7;
+      outline:    none;
+      border:     none;
+    }
+  
+    &:active {
+      transform: translateY(0.5px);
+    }
+  `;
+  
+  
+  type ButtonProps = {
+    children?:     any;
+    className?:    string;
+    disabled?:     boolean;
+    hide?:         boolean;
+    isLoading?:    boolean;
+    onClick?:      React.MouseEventHandler<HTMLButtonElement>;
+    open?:         boolean;
+  };
+  
+  const InnerContent = styled.div`
+    display:         flex;
+    flex-direction:  row;
+    position:        relative;
+    align-items:     center;
+    justify-content: center;
+    gap:             0.5rem;
+  `;
--- notes/src/components/sidePanel.tsx
+++ notes/src/components/sidePanel.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { ReactNode, useRef } from 'react';
+import styled from 'styled-components';
+import { useClickOutside } from '../util';
 
-export const SidePanel = () => {
+type SidePanelProps = {
+  open?:                () => void;
+  close?:               (open: boolean) => void;
+  showSidePanel?:       boolean;
+  children?:            ReactNode;
+  closeOnClickOutside?: boolean;
+};
+
+export const SidePanel = (props: SidePanelProps) => {
+  const { open, showSidePanel, children } = props;
+    
   return (
     <div>
-      Panel
+      <PanelContainer showSidePanel={showSidePanel} onClick={open}>
+        {children}
+      </PanelContainer>
     </div>
   );
 }
 
+
+const PanelContainer = styled.div<SidePanelProps>`
+  position:         absolute;
+  background-color: gray;
+  padding:          1rem 1rem;
+  z-index:          10;
+  opacity:          0.8;
+  top:              0;
+  left:             0;
+  height:           100vh;
+  width:            32rem;
+  transform:        ${(p) => p.showSidePanel ? "translateX(0%)" : "translate(-100%)"};
+  transition:       all 0.4s;
+`
