5fbf2cbe5a5cadbc7d0214e9805248f1a9b1947d
Agnes94 <agnes.ketesdi@gmail.com>
2022-07-27 21:25:50 +0200

    set up global styles

--- notes/src/App.tsx
+++ notes/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Button } from './components/button';
 import { SidePanel } from './components/sidePanel';
+import { GlobalStyle } from './globalStyles';
 
 export const App = () => {
+
+  const [showSidePanel, setShowSidePanel] = React.useState<boolean>(false);
+
+  const toggleSidePanel = React.useCallback(() => setShowSidePanel(!showSidePanel), [showSidePanel]);
+
   return (
     <div>
-      Hello!
-      <SidePanel/>
-      <Button>Hello</Button>
+      <GlobalStyle />
+      <SidePanel showSidePanel={showSidePanel} />
+      <Button onClick={toggleSidePanel}>Open</Button>
     </div>
   );
 }
